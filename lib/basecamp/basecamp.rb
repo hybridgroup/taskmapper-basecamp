716b6d7ac854ba3c976ab4951f863a26312e1069
Hong <hong.quach@abigfisch.com>
2010-07-20 18:24:43 -0700

    silencing activeresource warning for basecamp.rb

--- lib/basecamp/basecamp.rb
+++ lib/basecamp/basecamp.rb
@@ -18,14 +18,14 @@ ERROR
 end
 
 begin
-  require 'activeresource'
+  require 'active_resource'
 rescue LoadError
   begin
     require 'rubygems'
-    require 'activeresource'
+    require 'active_resource'
   rescue LoadError
     abort <<-ERROR
-The 'activeresource' library could not be loaded. If you have RubyGems 
+The 'active_resource' library could not be loaded. If you have RubyGems 
 installed you can install ActiveResource by doing "gem install activeresource".
 ERROR
   end
