037594d4db11d43ac4a8580dee16ce76c630cd67
Omar Rodriguez <omarjavier15@gmail.com>
2012-04-11 11:48:20 +0000

    save method implemente, not yet working

--- lib/provider/ticket.rb
+++ lib/provider/ticket.rb
@@ -13,7 +13,6 @@ module TicketMaster::Provider
     # * requestor => creator_name (read-only)
     # * project_id
     class Ticket < TicketMaster::Provider::Base::Ticket
-      attr_accessor :list
 
       def initialize(*options)
         @system_data ||= {}
@@ -52,16 +51,16 @@ module TicketMaster::Provider
         def create(attributes_hash)
           todo_item_hash = create_todo_item_hash attributes_hash
           todo_item = create_todo_item todo_item_hash
-          
+
           return nil unless todo_item.save
-          
+
           todo_item.project_id = attributes_hash[:project_id]
           todo_item.todo_list_id = todo_item_hash[:todo_list_id]
           self.new(todo_item)
         end
 
         private
-        
+
         def create_todo_item_hash(ticket_hash)
           a = ticket_hash
           validate_ticket_hash a
@@ -69,16 +68,16 @@ module TicketMaster::Provider
             :content => a[:title],
             :position => a[:priority] ||= 1,
             :todo_list_id => a[:todo_list_id] ||= create_todo_list({
-              :project_id => a[:project_id], 
-              :name => "#{a[:title]} list"  
-            }).id
+            :project_id => a[:project_id], 
+            :name => "#{a[:title]} list"  
+          }).id
           }
         end
-        
+
         def create_todo_item(attributes_hash)
           BasecampAPI::TodoItem.new(attributes_hash)
         end
-        
+
         def validate_ticket_hash(attributes_hash)
           title = attributes_hash[:title]
           raise ArgumentError.new "Title is required" if title.nil? or title.empty?
@@ -88,53 +87,68 @@ module TicketMaster::Provider
           BasecampAPI::TodoList.create(attributes)
         end
       end
+
+      def save
+        todo_item = BasecampAPI::TodoItem.find id, :params => { :todo_list_id => list.id }
+        copy_to(todo_item)
+        todo_item.save
+      end
+
+      def copy_to(todo_item)
+        todo_item.completed = status
+        todo_item.position = priority
+        todo_item.name = title
+        todo_item.completed = resolution
+        todo_item.responsible_party_name = assignee
+        todo_item.creator_name = requestor
+      end
+
+
+      def status
+        self.completed ? 'completed' : 'incomplete'
+      end
+
+      def priority
+        self.position
+      end
+
+      def priority=(pri)
+        self.position = pri
+      end
+
+      def title
+        self.content
+      end
+
+      def title=(titl)
+        self.content = titl
+      end
+
+      def updated_at=(comp)
+        self.completed_on = comp
+      end
+
+      def description
+        self.content
+      end
+
+      def description=(desc)
+        self.content = desc
+      end
+
+      def assignee
+        self.responsible_party_name
+      end
+
+      def requestor
+        self.creator_name
+      end
+
+      def comment!(*options)
+        options[0].merge!(:todo_item_id => id) if options.first.is_a?(Hash)
+        self.class.parent::Comment.create(*options)
+      end
+
     end
-
   end
-
-  def status
-    self.completed ? 'completed' : 'incomplete'
-  end
-
-  def priority
-    self.position
-  end
-
-  def priority=(pri)
-    self.position = pri
-  end
-
-  def title
-    self.content
-  end
-
-  def title=(titl)
-    self.content = titl
-  end
-
-  def updated_at=(comp)
-    self.completed_on = comp
-  end
-
-  def description
-    self.content
-  end
-
-  def description=(desc)
-    self.content = desc
-  end
-
-  def assignee
-    self.responsible_party_name
-  end
-
-  def requestor
-    self.creator_name
-  end
-
-  def comment!(*options)
-    options[0].merge!(:todo_item_id => id) if options.first.is_a?(Hash)
-    self.class.parent::Comment.create(*options)
-  end
-
 end
