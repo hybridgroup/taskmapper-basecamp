3b9149fe021e26431e4f687ea4a337a8a5652d7a
Rafael George <george.rafael@gmail.com>
2012-05-03 20:11:32 +0000

    refactor project_specs * now the specs are more cleaner

--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -1,72 +1,83 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Basecamp::Project" do
-  before(:all) do
-    headers = {'Authorization' => 'Basic MDAwMDAwOkJhc2VjYW1w'}
-    wheaders = headers.merge('Content-Type' => 'application/xml')
-    @project_id = 5220065
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get '/projects.xml', headers, fixture_for('projects'), 200
-      mock.get '/projects/5220065.xml', headers, fixture_for('projects/5220065'), 200
-      mock.get '/projects/create.xml', headers, fixture_for('projects/create'), 200
-      mock.delete '/projects/5220065.xml', headers, '', 200
-      mock.put '/projects/5220065.xml', wheaders, '', 200
-      mock.post '/projects.xml', wheaders, '', 201, 'Location' => '/projects/create.xml'
+
+  before(:all) do 
+    @headers =  {'Authorization' => 'Basic MDAwMDAwOkJhc2VjYW1w'}
+    @wheaders = @headers.merge('Content-Type' => 'application/xml') 
+  end
+
+  let(:tm) { TicketMaster.new(:basecamp, :token => '000000', :domain => 'ticketmaster.basecamphq.com') }
+  let(:project_class) { TicketMaster::Provider::Basecamp::Project }
+  let(:project_id) { 5220065 }
+
+  describe "Retrieving projects" do 
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/projects.xml', @headers, fixture_for('projects'), 200
+        mock.get '/projects/5220065.xml', @headers, fixture_for('projects/5220065'), 200
+      end
+    end
+
+    context "when calling #projects" do 
+      subject { tm.projects }
+      specify { subject.should be_an_instance_of(Array) }
+      specify { subject.first.should be_an_instance_of(project_class) }
+    end
+
+    context "when calling #projects using an array of id's" do 
+      subject { tm.projects([project_id]) }
+      specify { subject.should be_an_instance_of(Array) }
+      specify { subject.first.should be_an_instance_of(project_class) }
+      specify { subject.first.id.should be_eql(project_id) }
+    end
+
+    context "when calling #projects with a hash of project attributes" do 
+      subject { tm.projects(:id => project_id) }
+      specify { subject.should be_an_instance_of(Array) }
+      specify { subject.first.should be_an_instance_of(project_class) }
+      specify { subject.first.id.should be_eql(project_id) }
     end
   end
-  
-  before(:each) do
-    @ticketmaster = TicketMaster.new(:basecamp, :token => '000000', :domain => 'ticketmaster.basecamphq.com')
-    @klass = TicketMaster::Provider::Basecamp::Project
+
+  describe "Retrieve a single project" do 
+    before(:each) do
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/projects.xml', @headers, fixture_for('projects'), 200
+        mock.get '/projects/5220065.xml', @headers, fixture_for('projects/5220065'), 200
+      end
+    end
+
+    context "when calling #project with an id" do 
+      subject { tm.project(project_id) }
+      specify { should be_an_instance_of(project_class) }
+      specify { subject.id.should be_eql(project_id) }
+    end
+
+    context "when calling #project with project attributes" do 
+      subject { tm.project(:id => project_id) }
+      specify { should be_an_instance_of(project_class) } 
+      specify { subject.id.should be_eql(project_id) }
+    end
   end
-  
-  it "should be able to load all projects" do
-    @ticketmaster.projects.should be_an_instance_of(Array)
-    @ticketmaster.projects.first.should be_an_instance_of(@klass)
-  end
-  
-  it "should be able to load projects from an array of ids" do
-    @projects = @ticketmaster.projects([@project_id])
-    @projects.should be_an_instance_of(Array)
-    @projects.first.should be_an_instance_of(@klass)
-    @projects.first.id.should == @project_id
-  end
-  
-  it "should be able to load all projects from attributes" do
-    @projects = @ticketmaster.projects(:id => @project_id)
-    @projects.should be_an_instance_of(Array)
-    @projects.first.should be_an_instance_of(@klass)
-    @projects.first.id.should == @project_id
-  end
-  
-  it "should be able to find a project" do
-    @ticketmaster.project.should == @klass
-    @ticketmaster.project.find(@project_id).should be_an_instance_of(@klass)
-  end
-  
-  it "should be able to find a project by id" do
-    @ticketmaster.project(@project_id).should be_an_instance_of(@klass)
-    @ticketmaster.project(@project_id).id.should == @project_id
-  end
-  
-  it "should be able to find a project by attributes" do
-    @ticketmaster.project(:id => @project_id).id.should == @project_id
-    @ticketmaster.project(:id => @project_id).should be_an_instance_of(@klass)
-  end
-  
-  it "should be able to update and save a project" do
-    pending
-    @project = @ticketmaster.project(@project_id)
-    @project.save.should == nil
-    @project.update!(:name => 'some new name').should == true
-    @project.name = 'this is a change'
-    @project.save.should == true
-  end
-  
-  it "should be able to create a project" do
-    pending
-    @project = @ticketmaster.project.create(:name => 'Project #1')
-    @project.should be_an_instance_of(@klass)
+
+  describe "Creation and update" do 
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock| 
+        mock.post '/projects.xml', @wheadesr, '', 200
+        mock.get '/projects/5220065.xml', @headers, fixture_for('projects/5220065'), 200
+      end
+    end
+
+    context "when calling #project!" do 
+      subject { tm.project!(:name => 'Project #1') }
+      pending { should be_an_instance_of(project_class) }
+    end
+
+    context "when changing a project instance and calling #save" do 
+      subject { tm.project(project_id) }
+      specify { subject.save.should be_false }
+    end
   end
 
 end
