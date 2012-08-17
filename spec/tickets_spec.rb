6a473531a29ca37eeb645726849bed727615f479
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-17 15:18:53 +0000

    Refactor: Ticket specs *Remove ticket_id var, in specs been clear is mor important than been DRY (They are supposed to be easier to read than a program) *Share sample group added

--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -5,38 +5,18 @@ describe TaskMapper::Provider::Basecamp::Ticket do
   let(:headers) { {'Authorization' => 'Basic MDAwMDAwOkJhc2VjYW1w'} }
   let(:wheaders) { headers.merge('Content-Type' => 'application/xml') }
   let(:project) { tm.project(project_id) }
-  let(:ticket_id) { 133184178 }
   let(:tm) { TaskMapper.new(:basecamp, :domain => 'ticketmaster.basecamphq.com', :token => '000000') }
   let(:ticket_class) { TaskMapper::Provider::Basecamp::Ticket }
 
   context "Retrieve tickets" do 
-    before(:each) do 
+    before(:all) do 
       ActiveResource::HttpMock.respond_to do |mock|
         mock.get '/projects/5220065.xml', headers, fixture_for('projects/5220065'), 200
         mock.get '/todo_lists.xml?responsible_party=', headers, fixture_for('todo_list_with_items'), 200
       end
     end
-
-    describe :tickets do 
-      let(:tickets) { project.tickets }
-      subject { tickets }
-
-      it { should be_an_instance_of(Array) }
-      describe :first do 
-        its(:first) { should be_an_instance_of(ticket_class) }
-      end
-
-      describe "#tickets with an array of id's" do 
-        let(:tickets) { project.tickets [ticket_id] }
-        subject { tickets }
-
-        it { should be_an_instance_of Array }
-      end
-    end
-
-    describe :ticket do 
-      subject { project.ticket ticket_id }
-      it { should be_an_instance_of ticket_class }
+    
+    shared_examples_for "ticket 133184178" do
       its(:id) { should == 133184178  }
       its(:title) { should match /updated/ }
       its(:priority) { should == 1 }
@@ -49,6 +29,35 @@ describe TaskMapper::Provider::Basecamp::Ticket do
       its(:requestor) { should match /Clutch/ }
       its(:project_id) { should == 5220065 }
     end
+
+    describe "Reatrieve all" do 
+      let(:tickets) { project.tickets }
+      subject { tickets }
+
+      its(:count) { should == 4 }
+      
+      describe :first do 
+        subject { tickets.first }
+        it_behaves_like "ticket 133184178"
+      end
+    end
+    
+    pending "Search passing ids array'" do
+      let(:tickets) { project.tickets [133184178, 133180422] }
+      subject { tickets }
+      
+      its(:count) { should == 2 }
+      
+      describe :first do 
+        subject { tickets.first }
+        it_behaves_like "ticket 133184178"
+      end
+    end
+
+    describe "Find by id" do 
+      subject { project.ticket 133184178 }
+      it_behaves_like "ticket 133184178"
+    end
   end
 
   pending "Update and creation" do 
@@ -60,7 +69,7 @@ describe TaskMapper::Provider::Basecamp::Ticket do
     end
 
     context "when an instance of ticket is changed and then called the save method" do 
-      subject { project.ticket(ticket_id) } 
+      subject { project.ticket(133184178) } 
       pending { subject.save.should be_false }
     end
 
