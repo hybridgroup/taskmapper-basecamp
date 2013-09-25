4ded48b72811bf7a23bc23f66ded6415084853ff
Andrew Stewart <andrew@stwrt.ca>
2013-09-25 12:48:58 -0700

    Switch to simpler RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,6 @@ require 'active_resource/http_mock'
 require 'taskmapper-basecamp'
 require 'rspec'
 
-RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter = 'documentation'
-end
-
 def fixture_for(name, format = 'xml')
   File.read(File.dirname(__FILE__) + "/fixtures/#{name}.#{format}")
 end
