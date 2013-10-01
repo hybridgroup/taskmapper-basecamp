975b89961142122ce0e04ed3691c11133d98e2f4
Andrew Stewart <andrew@stwrt.ca>
2013-10-01 16:24:42 -0700

    Clean up provider requires

--- lib/taskmapper-basecamp.rb
+++ lib/taskmapper-basecamp.rb
@@ -23,6 +23,7 @@ class Hash
   end
 end
 
-%w{ basecamp ticket project comment }.each do |f|
-  require File.dirname(__FILE__) + '/provider/' + f + '.rb';
-end
+require 'provider/basecamp'
+require 'provider/project'
+require 'provider/ticket'
+require 'provider/comment'
